9e81633c7b4ec76de9ae9f025c157ac71ced0b92
Sebastian Balay <snbalay@gmail.com>
2017-05-15 16:35:21 -0300

    Redux setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import App from './screens/App';
 import './config/reactotron';
 
+import store from './redux/store';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/redux/store.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import Reactotron from 'reactotron-react-js';
+import thunk from 'redux-thunk';
+
+const reducers = combineReducers({});
+
+const middlewares = [];
+const enhancers = [];
+
+/* ------------- Thunk Middleware ------------- */
+middlewares.push(thunk);
+
+/* ------------- Assemble Middleware ------------- */
+enhancers.push(applyMiddleware(...middlewares));
+
+// in dev mode, we'll create the store through Reactotron
+const createAppropriateStore = process.env.NODE_ENV === 'development' ? Reactotron.createStore : createStore;
+const store = createAppropriateStore(reducers, compose(...enhancers));
+
+export default store;
