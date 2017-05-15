73c37077ec33d00368c5d67bb1027c50e6bc364c
Sebastian Balay <snbalay@gmail.com>
2017-05-15 15:18:13 -0300

    ESLint setup with commit prehook

--- src/screens/App/index.js
+++ src/screens/App/index.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import logo from '../../assets/logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+export default function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
       </div>
-    );
-  }
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
 }
-
-export default App;
