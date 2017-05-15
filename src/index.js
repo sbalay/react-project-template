73a11700339ebce882c0a1923b2f2aada2cf008e
mvbattan <manuel.battan@wolox.com.ar>
2017-05-15 15:17:04 -0300

    Installing react-router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+
+import createBrowserHistory from 'history/createBrowserHistory';
+
+const history = createBrowserHistory();
 
 import App from './screens/App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={history}>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
