70c7cba6f9d6710504484816318f2f1d7a45ac22
Sebastian Balay <snbalay@gmail.com>
2017-05-16 14:14:57 -0300

    Add redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,8 +17,10 @@ middlewares.push(thunk);
 /* ------------- Assemble Middleware ------------- */
 enhancers.push(applyMiddleware(...middlewares));
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line no-underscore-dangle
+
 // in dev mode, we'll create the store through Reactotron
 const createAppropriateStore = process.env.NODE_ENV === 'development' ? Reactotron.createStore : createStore;
-const store = createAppropriateStore(reducers, compose(...enhancers));
+const store = createAppropriateStore(reducers, composeEnhancers(...enhancers));
 
 export default store;
